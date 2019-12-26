029a4d61fb40fd4481429d575e30afa4eacd04ea
Ahsan Ejaz <33836928+AhsanE@users.noreply.github.com>
2019-12-26 15:00:10 +0500

    Update backdrop.js

--- backdrop.js
+++ backdrop.js
@@ -12,36 +12,38 @@
  * @param {boolean} scroller
  */
 function backdrop(backdropsource, backdropapply, filter, scroller) {
-    let backdropframe // reserved variable
+    let backdropframe; // reserved variable
     if (document.getElementById('iniframe') == null) {
-        backdropframe = document.createElement('iframe')
-        backdropframe.style.border = 'none'
-        backdropframe.width = '100%'
-        backdropframe.height = '100%'
-        backdropapply.appendChild(backdropframe)
-        addbackdrop()
+        backdropframe = document.createElement('iframe');
+        backdropframe.style.border = 'none';
+        backdropframe.width = '100%';
+        backdropframe.height = '100%';
+        backdropapply.appendChild(backdropframe);
+        addbackdrop();
     }
 
     function addbackdrop() {
-        const backdropdoc = backdropframe.contentWindow.document
-        backdropdoc.open()
-        backdropdoc.write(backdropsource)
-        var markup =
-            '<div id="iniframe"></div><style>body {overflow: hidden; -webkit-filter:' + filter + ';filter:' + filter + '; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none;}</style>'
-        backdropdoc.write(markup)
-        backdropdoc.close()
+        const backdropdoc = backdropframe.contentWindow.document;
+        backdropdoc.open();
+        backdropdoc.write(backdropsource);
+        let markup = `<div id="iniframe"/>body{overflow:hidden;-webkit-filter:${filter};filter:${filter};-webkit-touch-callout:none;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none; user-select:none;}</style>`;
+        backdropdoc.write(markup);
+        backdropdoc.close();
         if (scroller) {
             window.addEventListener(
                 'scroll',
                 function(event) {
-                    let scroll = this.scrollY
-                    let scrollx = this.scrollX
+                    let scroll = this.scrollY;
+                    let scrollx = this.scrollX;
                     let rect = backdropapply.getBoundingClientRect();
                     scroll = scroll - rect.top;
-                    backdropframe.contentWindow.window.scrollTo(scrollx, scroll)
+                    backdropframe.contentWindow.window.scrollTo(
+                        scrollx,
+                        scroll
+                    );
                 },
                 true
-            )
+            );
         }
     }
 }
